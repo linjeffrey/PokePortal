43aea3af0e6153929d9dff774025987cdbc5b0b1
Jeffrey <jefflin@berkeley.edu>
2015-03-25 02:01:41 -0700

    I am a Pokemon master."

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   root to: 'home#index'
   devise_for :trainers
   resources :trainers
+  resources :pokemon
+  patch "capture", to:"pokemon#capture"
+  patch "damage", to: "pokemon#damage"
+  get "/pokemon/new", to:"pokemon#new"
+  post "create", to:"pokemon#create"
 end
